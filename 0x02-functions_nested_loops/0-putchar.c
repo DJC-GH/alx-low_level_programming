d755ed13fa13c4a1b6bb92c69656252759a4709d
DJC-GH <danieljoshuachentiwuni1@gmail.com>
2023-03-21 22:12:31 -0700

    0-putchar file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
  * main - print _putchar
- * Return: Always 0.
  */
 
 int main(void)
 {
-	  char a[]="_putchar";
-  	 int b;
-        for(b=0; b<8; b++)
-        {
-        putchar(a[b]);
-        }
-	_putchar('\n');
+char a[]="_putchar";  	 
+int b; 
+for(b=0; b<8; b++)
+{
+putchar(a[b]);
+}
+_putchar('\n');
 return (0);
 }
